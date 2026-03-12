da6714adfacd261296333e2824be4c3477c221f7
Huyentsuk0176 <Phthihuyenta03@gmail.com>
2026-03-13 00:44:13 +0900

    module4_session7_ex4_kich hoat validation trong controller

---
+++ src/main/java/com/example/employee_api/student/controller/StudentController.java
@@ -0,0 +1,15 @@
+package com.example.employee_api.student.controller;
+
+import com.example.employee_api.student.dto.StudentDTO;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/students")
+public class StudentController {
+
+    @PostMapping
+    public String createStudent(@Valid @RequestBody StudentDTO studentDTO) {
+        return "Thêm sinh viên thành công";
+    }
+}
